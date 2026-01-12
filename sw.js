71f289abacf57270109ebe0adf3b93ce1047ac3b
phummiphat582-debug <phummiphat582@gmail.com>
2026-01-12 16:37:52 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('po-app').then(cache => {
-      return cache.addAll([
+    caches.open('po-app').then(cache =>
+      cache.addAll([
         '/-/',
         '/-/index.html',
         '/-/style.css',
         '/-/script.js'
-      ]);
-    })
+      ])
+    )
   );
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
